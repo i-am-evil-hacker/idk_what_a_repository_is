86df8c0a7b18b56cf661ae41287b4f62884ca230
i-am-evil-hacker <johanwarrior333@gmail.com>
2025-12-27 09:04:30 +0530

    Update dance.py

--- ehehehhehehfdskdshuds/dance.py
+++ ehehehhehehfdskdshuds/dance.py
@@ -1,28 +1,10 @@
-import time,threading
-n=0 
-a="|"
-b="\\"
-c="-"
-d="/"
-t=0.05
+import sys,time
+frames=list("abcdefghijklmnoqrstuvwxyz")
+def animate(frames,cd):
+    frames=frames[::-1]
+    for i in range(len(frames)):
+        sys.stdout.write((" "*(len(frames)-i))+frames[i]+"\r")
+        sys.stdout.flush()
+        time.sleep(cd)
 while True:
-    break
-    print(a*40)
-    time.sleep(t)
-    print(b*40)
-    time.sleep(t)
-    print(c*40)
-    time.sleep(t)
-    print(d*40)
-    time.sleep(t)
-def sum(nums,target):
-        l=[]
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    l.append(i,j)
-                    print(l)
-        return l
-for i in range(10):
-    time.sleep(0.5)
-    print("hello world\a")
+    animate(frames,0.1)
