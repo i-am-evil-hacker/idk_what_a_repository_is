bf9c6f91fe4fdd68416bd3374779c0af9132a364
not-ETHICAL-HACKER <johanwarrior0@gmail.com>
2025-12-27 08:31:57 +0530

    Refactor game logic and add Player class
    Commented out the original procedural game code and introduced a new Player class with attributes and methods for difficulty adjustment and leveling up. Added imports for terminal size, numpy, and typing, and seeded the random number generator for reproducibility.

--- DOOOOOOOONT TOUCH/test.py
+++ DOOOOOOOONT TOUCH/test.py
@@ -1,9 +1,9 @@
-def fib_rec(lim:int):
+def fib_rec(lim:int)->int:
     if lim in (1,2):
         return 1
     else:
         return fib_rec(lim-1)+fib_rec(lim-2)
-def fib(lim:int):
+def fib(lim:int)->int:
     a,b=0,1
     for _ in range(lim):
         a,b=b,a+b
